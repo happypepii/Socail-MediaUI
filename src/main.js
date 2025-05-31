30e7f9647ac6698ef45f398619448c117e92020a
Pei-Han Jiang <peihan945@gmail.com>
2025-05-31 18:11:28 +0800

    signup page done

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'http://localhost:8080'
 const app = createApp(App)
 
 app.use(createPinia())
